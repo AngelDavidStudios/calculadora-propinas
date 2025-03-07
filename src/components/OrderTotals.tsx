c1e8f6ca28e958c84092f0625848a5f9fa44d911
Angel David <angeldavidstudios@outlook.com>
2025-03-07 17:35:29 -0500

    Colocacion del useCallback

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react"
+import { useCallback, useMemo } from "react"
 import { OrderItem } from "../types"
 import { formatCurrency } from "../helpers"
 
@@ -11,11 +11,8 @@ type OrderTotalsProps = {
 export default function OrderTotals({order, tip, placeOrder}: OrderTotalsProps) {
 
     const subtotalAmount = useMemo(() => order.reduce((total, item) =>  total + (item.quantity * item.price), 0), [order])
-
     const tipAmount = useMemo(() => subtotalAmount * tip, [tip, order])
-
-    const totalAmount = useMemo(() => subtotalAmount + tipAmount, [tip, order])
-
+    const totalAmount = useCallback(() => subtotalAmount + tipAmount, [tip, order])
 
   return (
     <>
@@ -30,14 +27,14 @@ export default function OrderTotals({order, tip, placeOrder}: OrderTotalsProps) 
             </p>
 
             <p>Total a pagar: {''}
-                <span className=" font-bold">{formatCurrency(totalAmount)}</span>
+                <span className=" font-bold">{formatCurrency(totalAmount())}</span>
             </p>
 
         </div>
 
         <button
             className=" w-full bg-black p-3 uppercase text-white font-bold mt-10 disabled:opacity-10"
-            disabled={totalAmount === 0}
+            disabled={totalAmount() === 0}
             onClick={placeOrder}
             >
             Guardar Orden
